f30cd44112561807e746cccb1fec878aebeeac51
Topotam <gilles.lionel@gmail.com>
2022-03-12 05:31:28 +0100

    Update PetitPotam.py

--- PetitPotam.py
+++ PetitPotam.py
@@ -2,7 +2,7 @@
 # 
 # Author: GILLES Lionel aka topotam (@topotam77)
 # 
-# Greetz : grenadine(@Greynardine), skar(@__skar), didakt(@inf0sec1), plissken, pixis(@HackAndDo), shutd0wn(@ _nwodtuhs) et ptete '144.one' :D
+# Greetz : grenadine(@Greynardine), skar(@__skar), didakt(@inf0sec1), plissken, pixis(@HackAndDo)
 # "Most of" the code stolen from dementor.py from @3xocyte ;)
 
 
